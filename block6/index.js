281273684148ceea0280568116f32f1d1f9095dd
Semerbl4 <69350287+Semerbl4@users.noreply.github.com>
2020-11-06 15:29:25 +0300

    1.5 changes.
    Изменил макет по 1.4. , добавил сделанный свайпер по 1.5.

---
+++ block6/index.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 'auto',
+      spaceBetween: 16,
+      height: 50,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
