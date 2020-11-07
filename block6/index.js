7b4b809bde835d4412301cf93befd2ef585af6fb
Semerbl4 <69350287+Semerbl4@users.noreply.github.com>
2020-11-07 04:53:03 +0300

    Delete index.js

--- block6/index.js
+++
@@ -1,9 +0,0 @@
-var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 'auto',
-      spaceBetween: 16,
-      height: 50,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-    });
